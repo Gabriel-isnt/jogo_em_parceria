ec179618ddc1d4c8aabddaf750777ae4289084db
At_oa <85297718+Gabriel-isnt@users.noreply.github.com>
2024-01-27 11:10:59 -0300

    Create historia.py

---
+++ historia.py
@@ -0,0 +1,51 @@
+def historia_inicial():
+    print("--------------------------------------------")
+    print(" Está chovendo do lado de fora, são quase meia noite. \n\n Eu não devia ter aceitado jogar roleta-russa com meus colegas da faculdade, eu nem sei como eles conseguiram esse revolver... \n\n Agora eu estou aqui no banheiro enrolando para não encarar aquilo que eu escolhi para minha vida.")
+    input()
+    print("--------------------------------------------")
+
+    print("--------------------------------------------")
+    print(" Eu tô muito longe para pedir ajuda de qualquer pessoa minimamente descente, meu celular foi jogado no vaso pelo Victor, aquele bebado nojento que eu ainda chamo de amigo. \n\n Eu estou com muito medo de sair daqui, eu só não pulo a janela porque a queda até o chão seria feia.")
+    input()
+    print("--------------------------------------------")
+
+    print("--------------------------------------------")
+    print("Hora de encarar a realidade...")
+    input()
+    print("--------------------------------------------")
+
+    print("--------------------------------------------")
+    print(" Eu começo a abrir a porta e aquele cheiro forte de bebida e droga começam a tomar conta do meu nariz. \n\n Willian alugou uma casa online longe da \"civilização\" para que a galera pudesse fazer o que quisesse livremente, e eu escolhi logo isso...")
+    input()
+    print("--------------------------------------------")
+
+    print("--------------------------------------------")
+    print(" Todo mundo sabia que isso aconteceria, mas ninguém estava levando a sério. \n\n Todas essas pessoas, por causa das drogas que estão ingerindo, vão consentir na morte dos envolvidos e nem percebem. \n\n Não adianta gritar nem gesticular, é como se, eu podendo falar, ninguém me ouvisse.")
+    input()
+    print("--------------------------------------------")
+
+    print("--------------------------------------------")
+    print("\n Eu ando até a sala principal da casa. Todo este lugar está uma zona, desde a cozinha até o quintal tem pessoas fazendo algo que afronta até a moral da civilização mais ordinária que já passou pela terra. \n\n Na sala tem uma mesa redonda com 7 pessoas. \n\n Uma é o meu oponente, o restante está apenas como plateia que interage com o espetáculo que vai acontecer, mas, estão bebados demais para poderem perceber que, de fato, alguém vai morrer aqui dentro.")
+    input()
+    print("--------------------------------------------")
+
+def explicacao():
+    print("--------------------------------------------")
+    print(" Para esse bando de sádicos que estão aqui aproveitarem mais o jogo, eles decidiram mudar as regras do jogo. \n\n Ao invés de atirar na cabeça, a pessoa que está com a arma vai decidir se atira nela mesma ou no \"inimigo\". \n\n O tiro vai ser em alguma parte não letal do corpo para que a vitima jogue por mais tempo. \n\n E a arma será carregada com bala falsa (de chumbinho) e com bala de verdade, a falsa calsa uma leve dor apenas.")
+    input()
+    print("--------------------------------------------")
+
+    print("--------------------------------------------")
+    print(" Se a pessoa com a arma atirar nela mesma, ela pode atirar de novo, só é passada a vez para o outro se a pessoa atirar no oponente dela. \n\n Há objetos que as pessoas ao redor vão dar para cada um toda vez que a munição acabar. \n\n Esses objetos ajudam para a pessoa não morrer e para matar mais rápido o outro.") 
+    input()
+    print("--------------------------------------------")
+
+    print("--------------------------------------------")
+    print(" Tem um \"ROLO DE CÂMERA\", quem tiver um desse pode usar para saber se na sua vez tem uma bala ou não na arma. \n\n Outro objeto é a \"FACA\", a pessoa faz um leve corte nela mesma e poderá dar dois tiros de uma vez. \n\n Será dado também um \"ESPARADRAPO\", isso vai ser usado para parar o sangramento do tiro que você levar e por ultimo. \n\n Será dado um \"AMARRADOR\", a pessoa que for amarrada vai perder a vez.")
+    input()
+    print("--------------------------------------------")
+
+    print("--------------------------------------------")
+    print("Hora de \"aproveitar a vida da forma certa\"...")
+    input()
+    print("--------------------------------------------")
