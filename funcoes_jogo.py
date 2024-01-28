2b757676bb3fb5520ce6467b666304a36f546b86
At_oa <85297718+Gabriel-isnt@users.noreply.github.com>
2024-01-28 15:27:32 -0300

    Update funcoes_jogo.py
    Troquei de lista para dicionário porque facilitaria na criação da ideia do menu interativo que a Ana deu

--- funcoes_jogo.py
+++ funcoes_jogo.py
@@ -1,9 +1,10 @@
 import random as rd 
-
-
+        
 def erro(msg):  # usado pelo programador para avisar algo inválido ao usuário
     print(f"\033[91m{msg}\033[0m")
 
+def nome():
+    return str(input("digite o seu nome: "))[:7]  # só aceita 7 letras isso ai
 
 def item_surpresa(quantidade, inventario):
     # rolo de camera = revela para você se a bala é de chumbinho ou de verdade na sua vez
@@ -13,16 +14,25 @@ def item_surpresa(quantidade, inventario):
     # lápis = altera a munição da arma 
     itens = ('rolo de camera', 'esparadrapo', 'amarrador', 'faca', 'lápis')
 
-    pessoa = inventario
-    
-    for _ in range(0, quantidade):  # para colocar os itens no inventário
-       pessoa.append(rd.choice(itens))
+    # eu crio o dicionário para ele receber os valores antigos
+    # para ser adicionado mais coisa no primeiro for
+    item_usuário = inventario.copy()
+
+    # escolhe os itens aleatoriamente
+    itens_escolhidos = []    
+
+    for _ in range(0, quantidade):
+        itens_escolhidos.append(rd.choice(itens))
+
+    # adicionando item e quantidade automaticamente
+    for item in itens_escolhidos:
+        if item in item_usuário:
+            item_usuário[item] += 1
         
-    return pessoa
+        else:
+            # pq ai já cria com um valor normal
+            item_usuário[item] = 1
 
-def nome():
-    return str(input("digite o seu nome: "))[:7]  # só aceita 7 letras isso ai
-
-def altera_municao():
-    pass
+    # fim da função
+    return item_usuário
 
